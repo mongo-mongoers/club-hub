733441e66e69c590cd78a58fa6e70ab26abed5cc
pmmanuel315 <pmmanuel315@gmail.com>
2023-04-13 11:34:43 -1000

    created EventsCollection

---
+++ app/imports/api/events/Events.js
@@ -0,0 +1,27 @@
+import { Mongo } from 'meteor/mongo';
+import SimpleSchema from 'simpl-schema';
+
+/** Encapsulates state and variable values for this collection. */
+class EventsCollection {
+  constructor() {
+    // The name of this collection.
+    this.name = 'EventsCollection';
+    // Define the Mongo collection.
+    this.collection = new Mongo.Collection(this.name);
+    // Define the structure of each document in the collection.
+    this.schema = new SimpleSchema({
+      club: { type: String },
+      date: { type: String },
+      description: { type: String },
+      email: { type: String },
+      location: { type: String },
+    });
+    // Ensure collection documents obey schema.
+    this.collection.attachSchema(this.schema);
+    // Define names for publications and subscriptions
+    this.userPublicationName = `${this.name}.publication.user`;
+    this.adminPublicationName = `${this.name}.publication.admin`;
+  }
+}
+
+export const Events = new EventsCollection();
