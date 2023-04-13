fecb9c80e7a03d56b7a2fe3d658507276b105d7d
Cash Baker <cashbake@hawaii.edu>
2023-04-12 22:39:13 -1000

    Update Profiles.jsx

--- app/imports/ui/pages/Profiles.jsx
+++ app/imports/ui/pages/Profiles.jsx
@@ -2,27 +2,11 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useTracker } from 'meteor/react-meteor-data';
-// import { Profiles } from '../../api/profiles/Profiles';
-// import { ProfilesInterests } from '../../api/profiles/ProfilesInterests';
-// import { ProfilesProjects } from '../../api/profiles/ProfilesProjects';
-// import { Projects } from '../../api/projects/Projects';
 import LoadingSpinner from '../components/LoadingSpinner';
 import { pageStyle } from './pageStyles';
 import ClubCard from '../components/ClubCard';
 import { ProfilesClubs } from '../../api/profiles/ProfilesClubs';
 import { Clubs } from '../../api/clubs/Clubs';
-// import { Clubs } from '../../api/clubs/Clubs';
-
-/* Returns the Profile and associated Projects and Interests associated with the passed user email. */
-// function getProfileData(email) {
-//   const data = Profiles.collection.findOne({ email });
-//   const interests = _.pluck(ProfilesInterests.collection.find({ profile: email }).fetch(), 'interest');
-//   const projects = _.pluck(ProfilesProjects.collection.find({ profile: email }).fetch(), 'project');
-//   const projectPictures = projects.map(project => Projects.collection.findOne({ name: project })?.picture);
-//   // console.log(_.extend({ }, data, { interests, projects: projectPictures }));
-//   return _.extend({}, data, { interests, projects: projectPictures });
-// }
-
 /* Component for layout out a Profile Card. */
 
 /* Renders the Profile Collection as a set of Cards. */
@@ -40,20 +24,6 @@ const ProfilesPage = () => {
       clubData: clubs,
     };
   }, []);
-
-  /*
-  const clubData = {
-    clubs: [],
-    ready: false,
-  };
-  if (ready) {
-    const userProfilesClubs = ProfilesClubs.collection.find({ profileEmail: user.username }).fetch();
-    const userClubNames = userProfilesClubs.map((profileClub) => profileClub.clubName);
-    const clubs = userClubNames.map(clubName => Clubs.collection.find({ name: clubName }).fetch()[0]);
-    clubData.clubs = clubs;
-    clubData.ready = true;
-  }
-  */
   return ready ? (
     <Container style={pageStyle}>
       <Row className="justify-content-center">
